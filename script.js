ada853e5428c85ba2f37e7fedc85b19571e8267e
Fran Sanabria <fwilsonnavarro@gmail.com>
2024-06-17 16:34:57 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+import data from './data.json' assert {type: 'json'};
+for (let i=0; i<data.length; i++){
+    let icono = document.getElementById(`ic${[i]}`);
+    let cat = document.getElementById(`cat${[i]}`)
+    let scores = document.getElementById(`sc${[i]}`)
+    icono.setAttribute("src",data[i].icon)
+    cat.textContent = data[i].category
+    scores.textContent = data[i].score
+    scores.innerHTML += '<span> /100</span>'
+  }
